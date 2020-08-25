f7c51730629b2061a1421b4f53a043a8c3f2c94f
TheImaginaryOne <johnathanleung@hotmail.co.nz>
2020-08-25 14:09:09 +1200

    Add user class

---
+++ domainmodel/test/test_user.py
@@ -0,0 +1,40 @@
+from domainmodel.review import Review
+from domainmodel.movie import Movie
+from domainmodel.user import User
+
+
+def test_basic():
+    user1 = User('Martin', 'pw12345')
+    assert user1.user_name == "martin"
+    assert user1.__repr__() == "<User martin>"
+
+
+def test_add_movie():
+    movie = Movie("Shrek but every time he takes a STEP it gets 5% faster", 2010)
+    movie.runtime_minutes = 9
+    movie2 = Movie("The Bee Movie but its 20000x faster", 2010)
+    movie2.runtime_minutes = 3
+    user1 = User('Me', 'pw12345')
+
+    user1.watch_movie(movie)
+    assert user1.time_spent_watching_movies_minutes == 9
+    user1.watch_movie(movie2)
+    assert user1.time_spent_watching_movies_minutes == 12
+    assert user1.watched_movies == [movie, movie2]
+
+
+def test_user_equality():
+    user1 = User('Chewbacca', 'pp')
+    user2 = User('Chewbacca', 'pp')
+    user3 = User('a_bee', 'pp')
+    assert user1 == user2
+    assert user1 != user3
+
+
+def test_add_review():
+    user1 = User('Me', 'pw12345')
+    movie2 = Movie("Star Wars", 1977)
+
+    review = Review(movie2, "My car turned into the Millennium Falcon after showing the movie to it", 10)
+    user1.add_review(review)
+    assert user1.reviews == [review]
--- domainmodel/user.py
+++ domainmodel/user.py
@@ -1,5 +1,38 @@
 from domainmodel.movie import Movie
 from domainmodel.review import Review
 
+
 class User:
-    pass
+    def __init__(self, user_name, password):
+        self.user_name: str = user_name
+        self.password: str = password
+        self.watched_movies: [Movie] = []
+        self.reviews: [Review] = []
+        self.time_spent_watching_movies_minutes: int = 0
+
+    @property
+    def user_name(self):
+        return self._user_name
+
+    @user_name.setter
+    def user_name(self, u: str):
+        self._user_name = u.lower().strip()
+
+    def __repr__(self):
+        return f"<User {self._user_name}>"
+
+    def __eq__(self, other: 'User'):
+        return self._user_name == other._user_name
+
+    def __lt__(self, other: 'User'):
+        return self._user_name < other._user_name
+
+    def __hash__(self):
+        return hash(self._user_name)
+
+    def watch_movie(self, movie: Movie):
+        self.watched_movies.append(movie)
+        self.time_spent_watching_movies_minutes += movie.runtime_minutes
+
+    def add_review(self, review: Review):
+        self.reviews.append(review)
